d49d53ac55494b30f758449e085629299f1f3362
Michael Agard <mdagard@gmail.com>
2020-07-21 06:55:58 -0400

    add update script to get mine.lua

---
+++ update.lua
@@ -0,0 +1,12 @@
+
+BaseUrl = "https://github.com/michaelagard/computercraft_scripts/blob/"
+Branch = {"master"}
+Script = {"mine"}
+io.write("Updating")
+shell.run("wget", "https://github.com/michaelagard/computercraft_scripts/blob/master/mine.lua", Scripts[1] .. ".lua")
+
+function Update()
+    for i = 1, #Script, 1 do
+        shell.run("wget", "https://github.com/michaelagard/computercraft_scripts/blob/" .. Branch[1] .. Script[1] .. ".lua") 
+    end
+end
