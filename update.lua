9970d8afea1d81521c172cfc6944e5111b89f7f2
Michael Agard <mdagard@gmail.com>
2020-07-21 08:57:27 -0400

    add update to Script, and removed broken code

--- update.lua
+++ update.lua
@@ -1,20 +1,10 @@
 -- Usage: >update 
 BaseUrl = "https://raw.githubusercontent.com/michaelagard/computercraft_scripts"
 Branch = "master"
-Script = {"mine"}
+Scripts = {"mine", "update"}
 
-term.setTextColor(color.red)
-io.write("Updating\n")
-
-function Update()
-    for i = 1, #Script, 1 do
-        shell.run("wget", "https://github.com/michaelagard/computercraft_scripts/blob/" .. Branch[2] .. "/" .. Script[1] .. ".lua") 
-    end
+function UpdateScripts()
     
-end
-
-function DeletePreviousFile()
-    shell.run("rm", "mine.lua")
 end
 
 local args = {...}
@@ -27,5 +17,3 @@ end
 if (#args >= 2 and args[1] == "-b") then
     Branch = args[2]
 end
-
-print(Branch)
