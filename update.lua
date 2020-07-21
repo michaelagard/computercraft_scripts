72a4fde6a678ad5cae8bf4243bb0f08a9b4fb018
Michael Agard <mdagard@gmail.com>
2020-07-21 09:59:03 -0400

    refined code, debug mode check

--- update.lua
+++ update.lua
@@ -1,35 +1,46 @@
--- Usage: >update 
 BaseUrl = "https://raw.githubusercontent.com/michaelagard/computercraft_scripts"
 Branch = "master"
 Scripts = {"mine", "update"}
+Debug = true
+local args = {...}
 
 function UpdateScripts()
     
 end
 
-local args = {...}
-
 -- check args
+-- example commands:
+-- update all
+-- update mine
+-- update -b master all
+-- update -b dev mine
+
 if (#args == 0) then
-    io.write("Usage:\nupdate [-b branch] [scriptName1, ...]\nupdate all\n")
-
-elseif (#args[1] == "-b") then
+    io.write("Usage:\n")
+    io.write("update [OPTION]... [scriptName1, scriptName2, ...]")
+    io.write("Options:\n")
+    io.write(" -b, --branch - Specify repo branch.\n")
+    
+elseif (args[1] == "-b" or args[1] == "--branch") then
     Branch = args[2]
-end
 
-if (not(args[3] == "all")) then
+elseif (not(args[3] == "all" or args[1 == "all"])) then
     Scripts = {}
     for i = 3, #args, 1 do
         Scripts[i - 2] = args[i]
     end
 end
 
-print("Scripts")
-for i = 1, #Scripts, 1 do
-    print(Scripts[i])
-end
+--debug code
 
-print("Args")
-for i = 1, #args, 1 do
-    print(args[i])
+if (debug) then
+    print("Scripts")
+    for i = 1, #Scripts, 1 do
+        print(Scripts[i])
+    end
+
+    print("Args")
+    for i = 1, #args, 1 do
+        print(args[i])
+    end
 end
