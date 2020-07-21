ed43d8f2ea9db64f3a3399ca9ab0e8be83460d9f
Michael Agard <mdagard@gmail.com>
2020-07-21 09:22:17 -0400

    add debug code for getting script args

--- update.lua
+++ update.lua
@@ -9,11 +9,26 @@ end
 
 local args = {...}
 
--- check update arguments
+-- check args
 if (#args == 0) then
-    io.write("Usage:\nupdate [-b branch] [scriptName1, ...]\nupdate all")
+    io.write("Usage:\nupdate [-b branch] [scriptName1, ...]\nupdate all\n")
+
+elseif (#args[1] == "-b") then
+    Branch = args[2]
+    
+    if (not(#args[3] == "all")) then
+        for i = 3, #args, 1 do
+            Scripts[i - 2] = args[i]
+        end
+    end
 end
+
+
 
 if (#args >= 2 and args[1] == "-b") then
     Branch = args[2]
+end
+
+for i = 1, #Scripts, 1 do
+    print(Scripts[i])
 end
