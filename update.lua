5d86c49570f8584a14a807c412be992df2ed6adf
Michael Agard <mdagard@gmail.com>
2020-07-21 08:45:37 -0400

    remove update from Script and rm, dev print added

--- update.lua
+++ update.lua
@@ -1,7 +1,8 @@
-
+-- Usage: >update 
 BaseUrl = "https://raw.githubusercontent.com/michaelagard/computercraft_scripts"
-Branch = {"master", "dev-update-script"}
-Script = {"mine", "update"}
+Branch = "master"
+Script = {"mine"}
+
 term.setTextColor(color.red)
 io.write("Updating\n")
 
@@ -9,12 +10,22 @@ function Update()
     for i = 1, #Script, 1 do
         shell.run("wget", "https://github.com/michaelagard/computercraft_scripts/blob/" .. Branch[2] .. "/" .. Script[1] .. ".lua") 
     end
+    
 end
 
 function DeletePreviousFile()
     shell.run("rm", "mine.lua")
-    shell.run("rm", "update.lua")
 end
 
-DeletePreviousFile()
-Update()
+local args = {...}
+
+-- check update arguments
+if (#args == 0) then
+    io.write("Usage:\nupdate [-b branch] [scriptName1, ...]\nupdate all")
+end
+
+if (#args >= 2 and args[1] == "-b") then
+    Branch = args[2]
+end
+
+print(Branch)
