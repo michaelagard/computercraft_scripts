d5f6b00a56784ed2657b36953a3a5ea5de10f67d
Michael Agard <mdagard@gmail.com>
2020-07-22 10:34:14 -0400

    clean code, set default branch updateSettings function moved in logical if statement

--- update.lua
+++ update.lua
@@ -1,6 +1,7 @@
 local settings = {
     ["baseURL"] = "https://raw.githubusercontent.com/michaelagard/computercraft_scripts/",
-    ["branch"] = {"master"},
+    ["default_branch"] = {"master"},
+    ["branch"] = {},
     ["flags"] = {
         ["-a"] = {["arguments"] = {}, ["passed"] = false, ["rel_flag"] = "--all", ["rel_setting"] = "scripts"},
         ["-b"] = {["arguments"] = {}, ["passed"] = false, ["rel_flag"] = "--branch", ["rel_setting"] = "branch"},
@@ -10,7 +11,8 @@ local settings = {
         ["--script"] = {["arguments"] = {}, ["passed"] = false, ["rel_flag"] = "-s", ["rel_setting"] = "scripts"},
         ["--debug"] = {["arguments"] = {}, ["passed"] = false, ["rel_flag"] = "--debug", ["rel_setting"] = "debug"},
     },
-    ["scripts"] = {"mine", "update"},
+    ["default_scripts"] = {"mine", "update"},
+    ["scripts"] = {},
     ["debug"] = false,
     ["error"] = {},
     ["error_type"] = {
@@ -22,35 +24,62 @@ local settings = {
     }
 }
 
-local function debug() return settings.debug == true end
-local function addError(type, arg) table.insert(settings.error, settings.error_type[type](arg)) end
-local function validFlag(flag) return settings.flags[flag] ~= nil end
+local function debug()
+    return settings.debug == true
+end
+
+local function addError(type, arg)
+    table.insert(settings.error, settings.error_type[type](arg))
+end
+
+local function validFlag(flag)
+    return settings.flags[flag] ~= nil
+end
+
 local function addFlagToIgnored(flag)
     settings.flags[flag].passed = true
     settings.flags[settings.flags[flag].rel_flag].passed = "ignored"
 end
-local function flagIgnored(flag) return settings.flags[flag].passed end
+
+local function flagIgnored(flag)
+    return settings.flags[flag].passed
+end
+
 local function addArgumentToFlag(flag, arg)
     if (debug()) then
         print("addArgumentToFlag", flag, arg)
     end
+
     table.insert(settings.flags[flag].arguments, arg)
 end
 
 local function updateSettings()
-    for key, value in pairs(settings.flags) do
-        if settings.flags[key].passed == true then
+    for flag, value in pairs(settings.flags) do
+        if (settings.flags[flag].passed == true) then
             local newSettings = {}
-            for i = 1, #settings.flags[key].arguments, 1 do
+            local argument = settings.flags[flag].arguments
+
+            for i = 1, #argument, 1 do
                 if (debug()) then
-                    print("updateSettings()", settings.flags[key].rel_setting, settings.flags[key].arguments[i])
+                    print("updateSettings()", settings.flags[flag].rel_setting, argument[i])
                 end
-                table.insert(newSettings, settings.flags[key].arguments[i])
+
+                table.insert(newSettings, argument[i])
             end
-            settings[settings.flags[key].rel_setting] = newSettings
+            
+            settings[settings.flags[flag].rel_setting] = newSettings
         end
     end
-    if settings.scripts[1] == nil then
+
+    if (settings.flags["-a"].passed == true) then
+        settings.scripts = settings.default_scripts
+    end
+
+    if (settings.flags["-b"].passed == false) then
+        settings.branch = settings.default_branch
+    end
+    
+    if (settings.scripts[1] == nil) then
         addError("no_script")
     end
 end
@@ -58,7 +87,7 @@ end
 local function updateScript(script)
 	local scriptUrl = settings.baseURL .. "/" .. settings.branch[1] .."/" .. script .. ".lua"
     local scriptPath = shell.dir() .. "/" .. script .. ".lua"
-    io.write("> Attempting to download '" .. script .. ".lua' from the " .. settings.branch[1] .. " branch.")
+    io.write("> Attempting to download '" .. script .. ".lua' from the " .. settings.branch[1] .. " branch.\n")
 	local remoteScript = http.get(scriptUrl)
     local localScript = fs.open(scriptPath, "w")
     
@@ -84,31 +113,34 @@ else
         end
 
         if (validFlag(args[i])) then
-            if args[i] == "--debug" then
+            if (args[i] == "--debug") then
                 settings.flags["--debug"].arguments = {true}
                 settings.debug = true
             end
+            
             if (debug()) then
                 print("Valid flag Found at index: " .. i, args[i])
             end
 
-            if flagIgnored(args[i]) then
+            if (flagIgnored(args[i])) then
                 addError("duplicate_flag", args[i])
             end
 
             addFlagToIgnored(args[i])
             CurrentArg = args[i]
+            
         elseif (CurrentArg ~= nil) then
             addArgumentToFlag(CurrentArg, args[i])
         end
     end
+    
+    updateSettings()
 
     if (#settings.error > 0) then
         for i = 1, #settings.error, 1 do
             error(settings.error[i])
         end
     else
-        updateSettings()
         for i = 1, #settings.scripts, 1 do
             updateScript(settings.scripts[i])
         end
