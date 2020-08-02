c28dcbf0c9ff776b19935ff5d036aa5ac70cd516
Michael Agard <mdagard@gmail.com>
2020-08-02 15:21:50 -0400

    add block count and fuel level

--- ok-dig.lua
+++ ok-dig.lua
@@ -1,11 +1,43 @@
-Message = ""
-while true do
-    term.clear()
-    io.write(Message)
-    if turtle.detect() then
-        Message = "Block found!"
-        turtle.dig()
+local message = ""
+local count = 0
+local wait_string = "I'm waiting on a block to mine!"
+local term_string = "Hold CTRL+T to terminate this program."
+local current_fuel = "Current Fuel:" .. turtle.getFuelLevel()
+local mined_blocks = {}
+
+local success, block_data = turtle.inspect()
+local inspected_block = block_data.name
+
+local function addCountToBlock(count, block)
+    if (mined_blocks[block] == nil) then
+        table.insert(mined_blocks, block)
+        mined_blocks[block][1] = count
     else
-        Message = "I'm waiting on a block to mine!\nHold CTRL+T to terminate this program."
+        mined_blocks[block][1] = mined_blocks[block][1] + count
+    end
+end
+
+local function writeMinedBlocks()
+    if not(mined_blocks == {}) then
+        io.write("No blocks have been mined.")
+    else
+        io.write("Blocks Mined:\n")
+        for block, count in pairs(mined_blocks) do
+            io.write(block .. ": " .. count .. "\n")
+        end
+    end
+end
+
+while true do
+    term.setCursorPos(1,1)
+    term.clear()
+    io.write(message)
+    if turtle.detect() then
+        message = inspected_block .. " found!"
+        turtle.dig()
+        addCountToBlock(1, inspected_block)
+        os.sleep(.5)
+    else
+        message = wait_string .. "\n" .. term_string .. "\n" .. current_fuel .. "\n" .. writeMinedBlocks()
     end
 end
