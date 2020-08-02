6f249002c64231b45388794ef8555c82b3b2d3d3
Michael Agard <mdagard@gmail.com>
2020-08-02 15:43:06 -0400

    test fix

--- ok-dig.lua
+++ ok-dig.lua
@@ -10,9 +10,9 @@ local mined_blocks = {}
 local function addCountToBlock(count, block)
     if (mined_blocks[block] == nil) then
         table.insert(mined_blocks, block)
-        mined_blocks[block][1] = count
+        mined_blocks[block] = count
     else
-        mined_blocks[block][1] = mined_blocks[block][1] + count
+        mined_blocks[block] = mined_blocks[block] + count
     end
 end
 
@@ -20,12 +20,11 @@ local function writeMinedBlocks()
     if mined_blocks == {} then
         return "No blocks have been mined."
     else
-        print("Blocks Mined:\n")
+        io.write("Blocks Mined:\n")
         for block, count in pairs(mined_blocks) do
-            print(block .. ": " .. tostring(count) .. "\n")
+            io.write(block .. ": " .. tostring(count) .. "\n")
         end
     end
-    return ""
 end
 
 while true do
@@ -38,7 +37,6 @@ while true do
         message = inspected_block .. " found!"
         turtle.dig()
         addCountToBlock(1, inspected_block)
-        os.sleep(1)
     else
         message = wait_string .. "\n" .. term_string .. "\n" .. current_fuel .. "\n" .. writeMinedBlocks()
     end
