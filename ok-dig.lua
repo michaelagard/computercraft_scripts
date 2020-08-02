b6706c4d718952d2863c1288d5a12d5b61f287e6
Michael Agard <mdagard@gmail.com>
2020-08-02 15:38:42 -0400

    fix success logic

--- ok-dig.lua
+++ ok-dig.lua
@@ -5,8 +5,7 @@ local term_string = "Hold CTRL+T to terminate this program."
 local current_fuel = "Current Fuel: " .. turtle.getFuelLevel()
 local mined_blocks = {}
 
-local success, block_data = turtle.inspect()
-local inspected_block = block_data.name
+
 
 local function addCountToBlock(count, block)
     if (mined_blocks[block] == nil) then
@@ -19,24 +18,27 @@ end
 
 local function writeMinedBlocks()
     if mined_blocks == {} then
-        io.write("No blocks have been mined.")
+        return "No blocks have been mined."
     else
-        io.write("Blocks Mined:\n")
+        print("Blocks Mined:\n")
         for block, count in pairs(mined_blocks) do
-            io.write(block .. ": " .. tostring(count) .. "\n")
+            print(block .. ": " .. tostring(count) .. "\n")
         end
     end
+    return ""
 end
 
 while true do
+    local success, block_data = turtle.inspect()
+    local inspected_block = block_data.name
     term.setCursorPos(1,1)
     term.clear()
     io.write(message)
-    if turtle.detect() then
+    if (success == true) then
         message = inspected_block .. " found!"
         turtle.dig()
         addCountToBlock(1, inspected_block)
-        os.sleep(.5)
+        os.sleep(1)
     else
         message = wait_string .. "\n" .. term_string .. "\n" .. current_fuel .. "\n" .. writeMinedBlocks()
     end
