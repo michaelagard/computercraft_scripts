dfc9625a20eb64cdb1aa3984f1e15ebdf5377b62
Michael Agard <mdagard@gmail.com>
2020-08-03 13:43:45 -0400

    fix logic error in addCountToBlock

--- ok-dig.lua
+++ ok-dig.lua
@@ -1,11 +1,9 @@
 local message = ""
 local count = 0
-local wait_string = "I'm waiting on a block to mine!"
-local term_string = "Hold CTRL+T to terminate this program."
-local current_fuel = "Current Fuel: " .. turtle.getFuelLevel()
+local wait_string = "STATUS: Observing.\n"
+local term_string = "Hold CTRL+T to terminate.\n"
+local current_fuel = "Current Fuel: " .. turtle.getFuelLevel() .. "\n"
 local mined_blocks = {}
-
-
 
 local function addCountToBlock(count, block)
     if (mined_blocks[block] == nil) then
@@ -17,35 +15,29 @@ local function addCountToBlock(count, block)
     print("added ", block, "+1")
 end
 
--- local function writeMinedBlocks()
---     local return_message = ""
---     local return_table = {}
---     if #mined_blocks < 1 then
---         return_message ="No blocks have been mined."
---     else
---         for key, value in pairs(mined_blocks) do
---             table.insert(return_table, key .. " " .. value)
---         end
---         return_message = table.concat(return_table, "\n")
---     end
---     io.write("Blocks Mined:\n")
---     return return_message
--- end
+local function writeMinedBlocks()
+    local return_message = ""
+    if #mined_blocks < 1 then
+        return_message ="No blocks have been mined."
+    else
+        io.write("Blocks Mined:")
+        return_message = table.concat(mined_blocks, "\n")
+    end
+    return return_message
+end
 
 while true do
     local success, block_data = turtle.inspect()
     local inspected_block = block_data.name
     term.setCursorPos(1,1)
     term.clear()
-    
     io.write(message)
+
     if (success == true) then
         message = inspected_block .. " found!"
         turtle.dig()
-        -- addCountToBlock(1, inspected_block)
-        print(block_data.name)
+        addCountToBlock(1, inspected_block)
     else
-        message = wait_string .. "\n" .. term_string .. "\n" .. current_fuel .. "\n"
+        message = wait_string .. term_string .. current_fuel .. writeMinedBlocks()
     end
-    -- io.write(writeMinedBlocks())
 end
