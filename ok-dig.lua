b04454eec654311784a910541855baa0ec7ea9b4
Michael Agard <mdagard@gmail.com>
2020-08-03 20:55:03 -0400

    added real fix and usage line if -h or --help

--- ok-dig.lua
+++ ok-dig.lua
@@ -6,6 +6,18 @@ local term_string = "Hold CTRL+T to terminate.\n"
 local current_fuel = "Current Fuel: " .. turtle.getFuelLevel() .. "\n"
 local mined_blocks = {}
 
+local function tableLength(table)
+    local count = 0
+    if type(table) == "table" then
+        for key, value in pairs(table) do
+            count = count + 1
+        end
+    return count
+    else
+        return 0
+    end
+end
+
 local function addCountToBlock(block_name)
     if (mined_blocks[block_name] == nil) then
         mined_blocks[block_name] = 1
@@ -24,7 +36,7 @@ end
 
 local function writeMinedBlocks()
     local return_message = ""
-    if #mined_blocks < 1 then
+    if tableLength(mined_blocks) < 1 then
         return_message ="No blocks have been mined."
     else
         return_message = "Blockes Mined:\n" .. table.concat(formattedBlockTable(mined_blocks), "\n")
@@ -32,11 +44,15 @@ local function writeMinedBlocks()
     return return_message
 end
 
-if (tonumber(args[1]) == nil) then
-    args[1] = 10000
+if (args[1] == nil) then
+    args[1] = 1000000
 end
 
 while mined_count < tonumber(args[1]) do
+    if args[1] == "--help" or args[1] == "-h" then
+        io.write("Usage: ok-dig [ammount to dig]\nAmount defaults to 1000000.")
+        break
+    end
     local success, block_data = turtle.inspect()
     local inspected_block = block_data.name
     term.setCursorPos(1,1)
