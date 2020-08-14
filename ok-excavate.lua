fdb93cf268d135693547aae43d4342b08956154d
Michael Agard <mdagard@gmail.com>
2020-08-14 11:30:09 -0400

    add tablelength, arg handle fix fuel math

--- ok-excavate.lua
+++ ok-excavate.lua
@@ -1,18 +1,4 @@
-Move = {
-    ["forward"] = {["command"] = function() turtle.forward(); Move.forward.count = Move.forward.count + 1; end, ["count"] = 0, ["reverse"] = "back"},
-    ["back"] = {["command"] = function() turtle.back() end, ["count"] = 0, ["reverse"] = "forward"},
-    ["turnLeft"] = {["command"] = function() turtle.turnLeft() end, ["count"] = 0, ["reverse"] = "turnRight"},
-    ["turnRight"] = {["command"] = function() turtle.turnRight() end, ["count"] = 0, ["reverse"] = "turnLeft"},
-    ["up"] = {["command"] = function() turtle.up() end, ["count"] = 0, ["reverse"] = "down"},
-    ["down"] = {["command"] = function() turtle.down() end, ["count"] = 0, ["reverse"] = "up"},
-    ["MoveSequence"] = {}
-}
-
-Settings = {
-    ["arg_x"] = 0, --right/left
-    ["arg_y"] = 0, --forward/backwards
-    ["arg_z"] = 0, --up/down
-}
+local args = {...}
 
 local function tableLength(table)
     local table_count = 0
@@ -25,17 +11,34 @@ local function tableLength(table)
     return 0
 end
 
-local args = {...}
+Move = {
+    ["forward"] = {["command"] = function() turtle.forward(); Move.forward.count = Move.forward.count + 1; end, ["count"] = 0, ["reverse"] = "back"},
+    ["back"] = {["command"] = function() turtle.back() end, ["count"] = 0, ["reverse"] = "forward"},
+    ["turnLeft"] = {["command"] = function() turtle.turnLeft() end, ["count"] = 0, ["reverse"] = "turnRight"},
+    ["turnRight"] = {["command"] = function() turtle.turnRight() end, ["count"] = 0, ["reverse"] = "turnLeft"},
+    ["up"] = {["command"] = function() turtle.up() end, ["count"] = 0, ["reverse"] = "down"},
+    ["down"] = {["command"] = function() turtle.down() end, ["count"] = 0, ["reverse"] = "up"},
+    ["MoveSequence"] = {}
+}
+
+Settings = {
+    --uses body relative directions
+    ["arg_x"] = 0, --right/left
+    ["arg_y"] = 0, --forward/backwards
+    ["arg_z"] = 0, --up/down
+    ["total_args"] = tableLength(args),
+}
 
 local function handleArguments()
+    if tableLength(args) > 0 then
     for i = 1, tableLength(args), 1 do
-        if (type(args[i]) == "number") then
+        if (type(tonumber(args[i])) == "number") then
             if (i == 1) then
-                Settings.arg_x = args[i]
+                Settings.arg_x = tonumber(args[i])
             elseif (i == 2) then
-                Settings.arg_y = args[i]
+                Settings.arg_y = tonumber(args[i])
             elseif (i == 3) then
-                Settings.arg_z = args[i]
+                Settings.arg_z = tonumber(args[i])
             else
                 error("too many arguments")
             end
@@ -43,6 +46,49 @@ local function handleArguments()
             error("argument is not a number.")
         end
     end 
+    else
+        print("Help line.")
+    end
 end
 
+local fuel_level = turtle.getFuelLevel()
+
+local function calculateRequiredFuel()
+    if tableLength(args) == 1 then
+        return Settings.arg_y * 4
+    elseif tableLength(args) == 2 then
+        return (Settings.args_y * Settings.args_x) * 4
+    elseif tableLength(args) == 3 then
+        return (Settings.args_y * Settings.args_x * Settings.arg_z) * 4
+    else
+        error("No args detected.")
+    end
+end
+
+local function traverseLevel()
+
+    for iX = 1, Settings.arg_x, 1 do
+
+        for iY = 1, Settings.arg_y, 1 do
+
+            if(turtle.detect()) then
+                turtle.dig()
+            end
+
+            Move.forward.command()
+        end
+        if (not(iX == Settings.arg_x)) then
+            Move.turnRight.command()
+    
+            if(turtle.detect()) then
+                turtle.dig()
+            end
+    
+            Move.forward.command()
+            Move.turnRight.command()
+        end
+    end
+end
+print(textutils.serializeJSON(args))
 handleArguments()
+print(calculateRequiredFuel())
