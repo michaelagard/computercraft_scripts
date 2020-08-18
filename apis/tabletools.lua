baee2b04d0656f977d3e279188a70749b854d4e5
Michael Agard <mdagard@gmail.com>
2020-08-18 15:07:09 -0400

    add external tabletools module

---
+++ apis/tabletools.lua
@@ -0,0 +1,10 @@
+local function length(table)
+    local table_count = 0
+    if (type(table) == "table") then
+        for key, value in pairs(table) do
+            table_count = table_count + 1
+        end
+        return table_count
+    end
+    error(table .. " is not a table.")
+end
